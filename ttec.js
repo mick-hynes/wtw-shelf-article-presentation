739cc02314b8e11d8f406fd20b9483be2504642c
mick-hynes <mick.hynes@ttecdigital.com>
2024-10-23 18:02:55 +0100

    Added input parameters for WTW web chat launch

---
+++ ttec.js
@@ -0,0 +1,60 @@
+// copyright Mick Hynes, TTEC Digital 2024
+
+
+$(document).ready(function() { 
+    // Pure JavaScript
+  
+    // subscribe to ready event
+    Genesys('subscribe', 'Messenger.ready', function () {
+      // subsribe to close widget event
+      console.log('READY: subscribing to open event...');
+  
+      Genesys("subscribe", "Messenger.opened", function(){
+        console.log('Messenger.open event invoked');
+      });
+  
+      console.log('READY: subscribing to conversationCleared event...');
+      Genesys('subscribe', 'MessagingService.conversationCleared', function(){
+        console.log('MessagingService.conversationCleared event invoked');
+        $('#wizardContainer').fadeIn();
+      });
+    
+      wireHandlers();
+
+      console.log('Opening form...')
+      $('#formContainer').fadeIn();
+      
+    });
+  });
+
+function wireHandlers(){
+    $('#launchGenesys').on( "click", function(e) {
+        e.preventDefault();
+        launchGenesys();
+    });
+
+}
+
+  function launchGenesys() {
+    console.log('Preparing Genesys Widget...');
+
+    Genesys('command', 'Database.set', {
+      messaging: {
+          customAttributes: {
+              memberForename:  $('#memberForename').val(),
+              bgroup: $('#memberBGroup').val(),
+              referenceNumber: $('#memberReferenceNumber').val(),
+              platform: $('#platform').val()
+          },
+      },
+  })
+
+    $('#wizardContainer').fadeOut();
+    
+        Genesys('command','Messenger.open',{},
+        function (o) {},
+        function (o) {
+            Genesys('command', 'Messenger.close')
+        }
+        )
+};
